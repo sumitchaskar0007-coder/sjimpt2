cc28269e2559a8e7aa571ef7fcde758332c915b5
sumitchaskar <sumitchaskar0007-coder@github.com>
2026-04-16 14:19:51 +0530

    ignored node_modules

--- src/api.js
+++ src/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: import.meta.env.VITE_API_URL || ' https://api.sjimt.in/api',
+  baseURL: import.meta.env.VITE_API_URL || '  https://api.jgefs.org/api',
   timeout: 30000,
 });
 
